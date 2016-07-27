c46b305d6d16074cca65d013257ea1aa32c9d370
Yordy Santos <yordywsantos@gmail.com>
2016-07-26 19:11:05 -0500

    Update Modulo.py

--- Modulo.py
+++ Modulo.py
@@ -23,7 +23,6 @@ def op(n,d,e,s):
     else:
         print("Ingrese una opcion segun nuestro Menu")
         pregunta1(n,d,e,s)
-
 def pregunta(n,d,e,s):
     p1=input("Usted desea  realizar otra operacion ?  [si]o[no] :")
     if (p1=='si' or p1=='Si' or p1=='SI'):
@@ -35,24 +34,61 @@ def pregunta(n,d,e,s):
     else:
         print("Ud debe tomar una decision ...")
         pregunta(n,d,e,s)
-
-def salir(n,d,e,s):
-    sal=input("Usted esta seguro que desea SALIR ?  [si]o[no] :")
-    if (sal=='si' or sal=='Si' or sal=='SI'):
-        print("")
-        print("Cerrando Sesion...")
-        
-    elif (sal=='no' or sal=='No' or sal=='NO'):
-        menu()
-        salir(n,d,e,s)
-    else:
-        print("Ud debe tomar una decision ...")
-        salir(n,d,e,s)
-
+def continuar():
+    print("¿Desea continuar?")
+    print("1.- si")
+    print("2.- no")
+    x=input("Elija una opcion : ")
+    if x=='1':
+        opciones()
+    elif x=='2':
+        salir()
+    else :
+        print("Intentelo de nuevo")
+        continuar()
+def salir():
+    print("¿Esta seguro de que desea salir?")
+    print("1.- si")
+    print("2.- no")
+    y=input("Elija una opcion : ")
+    if y=='2':
+        opciones()
+    elif y=='1':
+        print("El programa se esta cerrando...")
+        time.sleep(2)
+        sys.exit(1)
+    else :
+        print("Intentelo de nuevo")
+        salir()
 def verificar(n):
     for c in n:
         if(ord(c)<65 or  ord(c)>90) and (ord(c)<97 or  ord(c)>122) and (ord(c)!=32):
             return False
     return True
+def nnombre():
+    global nombre
+    nombre=input("Ingrese su nombre: ")
+    if (not verificar(nombre)):
+        print("Intentelo de nuevo")
+        nnombre()
+def napellido():
+    global apellido
+    apellido=input("Ingrese su apellido : ")
+    if (not verificar(apellido)):
+        print("Intentelo de nuevo")
+        napellido()
+def ndni():
+    global dni
+    while True :
+        try :
+            dni=int(input("Ingrese su DNI : "))
+            break
+        except :
+            print("Intentelo de nuevo")
+    dni=str(dni)
+    if (len(dni)!=8):
+        print("Intentelo de nuevo")
+        ndni()
+
     
     
