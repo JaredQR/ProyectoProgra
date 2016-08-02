220e481b93ac17d8acbe19acdf35cdfa149919df
JaredQR <jaredantonyqr@gmail.com>
2016-08-02 12:55:44 -0500

    Update Crucero.py

--- Crucero.py
+++ Crucero.py
@@ -1,12 +1,2 @@
-import os
-import sys
-import sqlite3
 
-def salida():
-    con=sqlite3.connect("Crucero.s3db")
-    cursor=con.cursor()
-    cursor.execute("SELECT * FROM Pais")
-    for i in cursor:
-        print("Codigo: ", i[0],"Nombre: ", i[1])
-salida()
     
