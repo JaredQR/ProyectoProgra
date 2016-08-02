908db83832fb2a4c931ed46ebae3c4df23b7e69d
JaredQR <jaredantonyqr@gmail.com>
2016-08-02 12:47:42 -0500

    Update programa.py

--- programa.py
+++ programa.py
@@ -1,18 +1,1 @@
-
-            DISENOBASE()
-        else:
-            print("No es una opcion valida")
-            DISENOBASE()
-            
-    elif opc==3:
-        barcos()
-    elif opc==4:
-        paquetes()
-    elif opc==5:
-       salir()
-    else:
-        print("Su opcion digitada no es valida ¡ ")
-        print()
-        DISENOBASE()
-
-DISENOBASE()   
+ 
